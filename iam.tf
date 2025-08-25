6a6a898c4619e33eba885b485a7e08d9291e1db6
Carlo D'Ambrosio <carlo@ql4b.net>
2025-08-25 06:28:27 +0200

    chore: stream to s3 setup

---
+++ iam.tf
@@ -0,0 +1,57 @@
+resource "aws_iam_role" "firehose_role" {
+  name = join("-", [module.this.id, "firehose-role" ])
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "firehose.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "firehose_policy" {
+  name = join("-", [module.this.id, "firehose-policy" ])
+  role = aws_iam_role.firehose_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+    #   {
+    #     Effect = "Allow"
+    #     Action = [
+    #       "es:ESHttpPost",
+    #       "es:ESHttpPut"
+    #     ]
+    #     Resource = "${var.opensearch_domain_arn}/*"
+    #   },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:AbortMultipartUpload",
+          "s3:GetBucketLocation",
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:ListBucketMultipartUploads",
+          "s3:PutObject"
+        ]
+        Resource = [
+          aws_s3_bucket.backup.arn,
+          "${aws_s3_bucket.backup.arn}/*"
+        ]
+      },
+    #   {s
+    #     Effect = "Allow"
+    #     Action = [
+    #       "lambda:InvokeFunction"
+    #     ]
+    #     Resource = aws_lambda_function.transform.arn
+    #   }
+    ]
+  })
+}
---
+++ sqs.tf
@@ -0,0 +1,66 @@
+# SQS Queue (the reliability/buffering layer)
+resource "aws_sqs_queue" "main" {
+  count = local.create_queue ? 1 : 0
+  name                       = join("-", [local.id, "pipeline"])
+  
+  visibility_timeout_seconds = local.queue_config.visibility_timeout_seconds
+  message_retention_seconds  = local.queue_config.message_retention_seconds
+  
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.dlq[0].arn
+    maxReceiveCount     = local.queue_config.max_receive_count
+  })
+  
+  tags = module.this.tags
+}
+
+# Dead Letter Queue
+resource "aws_sqs_queue" "dlq" {
+  count = local.create_queue ? 1 : 0
+  name                       = join("-", [local.id, "pipeline", "dlq"])
+  message_retention_seconds = 1209600  # 14 days
+  
+  tags = module.this.tags
+}
+
+# Queue policy for data sources
+resource "aws_sqs_queue_policy" "main" {
+  count     = local.create_queue && length(local.data_sources) > 0 ? 1 : 0
+  queue_url = aws_sqs_queue.main[0].id
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = [for source in local.data_sources : 
+            source.type == "sns" ? "sns.amazonaws.com" :
+            source.type == "eventbridge" ? "events.amazonaws.com" :
+            source.type == "api_gateway" ? "apigateway.amazonaws.com" :
+            "lambda.amazonaws.com"
+          ]
+        }
+        Action = "sqs:SendMessage"
+        Resource = aws_sqs_queue.main[0].arn
+        Condition = {
+          ArnEquals = {
+            "aws:SourceArn" = [for source in local.data_sources : source.arn]
+          }
+        }
+      }
+    ]
+  })
+}
+
+# For data source with type == "sns" subscribe the queue to sns topic
+resource "aws_sns_topic_subscription" "sqs" {
+  for_each = {
+    for idx, source in local.data_sources : idx => source
+    if source.type == "sns"
+  }
+  
+  topic_arn = each.value.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.main[0].arn
+}
