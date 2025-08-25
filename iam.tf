88efeaf7fd8a19b80fe80002b7722416e1f43670
Carlo D'Ambrosio <carlo@ql4b.net>
2025-08-26 00:28:01 +0200

    chore: cleanup

--- iam.tf
+++ iam.tf
@@ -85,3 +85,81 @@ resource "aws_iam_role_policy" "lambda_policy" {
     ]
   })
 }
+
+# IAM role for Firehose to write to OpenSearch
+resource "aws_iam_role" "firehose_opensearch" {
+  count = var.enable_opensearch ? 1 : 0
+  name  = join("-", [local.id, "firehose", "opensearch", "role"])
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "firehose.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "firehose_opensearch" {
+  count = var.enable_opensearch ? 1 : 0
+  name  = join("-", [local.id, "firehose", "opensearch", "policy"])
+  role  = aws_iam_role.firehose_opensearch[0].id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = concat([
+      {
+        Effect = "Allow"
+        Action = [
+          "es:DescribeElasticsearchDomain",
+          "es:DescribeElasticsearchDomains",
+          "es:DescribeElasticsearchDomainConfig",
+          "es:ESHttpPost",
+          "es:ESHttpPut",
+          "opensearch:DescribeDomain",
+          "opensearch:DescribeDomains",
+          "opensearch:DescribeDomainConfig",
+          "opensearch:ESHttpPost",
+          "opensearch:ESHttpPut"
+        ]
+        Resource = [
+          var.opensearch_config.domain_arn,
+          "${var.opensearch_config.domain_arn}/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:AbortMultipartUpload",
+          "s3:GetBucketLocation",
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:ListBucketMultipartUploads",
+          "s3:PutObject"
+        ]
+        Resource = [
+          aws_s3_bucket.backup.arn,
+          "${aws_s3_bucket.backup.arn}/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:PutLogEvents"
+        ]
+        Resource = aws_cloudwatch_log_group.firehose_opensearch[0].arn
+      }
+    ], local.enable_transform ? [{
+      Effect = "Allow"
+      Action = [
+        "lambda:InvokeFunction"
+      ]
+      Resource = module.transform[0].function_arn
+    }] : [])
+  })
+}
--- main.tf
+++ main.tf
@@ -1,20 +1,29 @@
 locals {
   id            = module.this.id
   context       = module.this.context
-  create_queue  = var.create_queue ? true : false
-  queue_config  = var.queue_config
-  queue_arn     = var.create_queue ? aws_sqs_queue.main[0].arn : var.existing_queue_arn
-  data_sources  = var.data_sources
 
-  transform = var.transform
-  enable_transform = var.enable_transform
-  transform_template = var.transform_template
-}
+  # data sources
+  data_sources  = var.data_sources
+  
+  # queue settings
+  create_queue        = var.create_queue ? true : false
+  queue_config        = var.queue_config
+  queue_arn           = var.create_queue ? aws_sqs_queue.main[0].arn : var.existing_queue_arn
+
+  # transform
+  enable_transform    = var.enable_transform
+  transform_template  = var.transform_template
+  transform           = var.transform
+
+  # opensearch
+  enable_opensearch   = var.enable_opensearch
+  opensearch_config   = var.opensearch_config
+  }
 
 # Firehose delivery stream
 resource "aws_kinesis_firehose_delivery_stream" "main" {
   name          = join("-", [local.id, "firehose"])
-  destination = var.enable_opensearch ? "opensearch" : "extended_s3"
+  destination   = local.enable_opensearch ? "opensearch" : "extended_s3"
 
   extended_s3_configuration {
     file_extension    = ".json"
@@ -47,11 +56,11 @@ resource "aws_kinesis_firehose_delivery_stream" "main" {
   }
 
   dynamic "opensearch_configuration" {
-    for_each = var.enable_opensearch ? [1] : []
+    for_each = local.enable_opensearch ? [1] : []
     content {
-      domain_arn = var.opensearch_config.domain_arn
-      role_arn   = aws_iam_role.firehose_opensearch[0].arn
-      index_name = var.opensearch_config.index_name
+      domain_arn =        local.opensearch_config.domain_arn
+      role_arn            = aws_iam_role.firehose_opensearch[0].arn
+      index_name          = var.opensearch_config.index_name
       
       buffering_size     = var.opensearch_config.buffering_size
       buffering_interval = var.opensearch_config.buffering_interval
@@ -97,101 +106,4 @@ resource "aws_kinesis_firehose_delivery_stream" "main" {
 # S3 bucket for failed records
 resource "aws_s3_bucket" "backup" {
   bucket = join("-", [local.id, "backup"])
-}
-
-# IAM role for Firehose to write to OpenSearch
-resource "aws_iam_role" "firehose_opensearch" {
-  count = var.enable_opensearch ? 1 : 0
-  name  = join("-", [local.id, "firehose", "opensearch", "role"])
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "firehose.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy" "firehose_opensearch" {
-  count = var.enable_opensearch ? 1 : 0
-  name  = join("-", [local.id, "firehose", "opensearch", "policy"])
-  role  = aws_iam_role.firehose_opensearch[0].id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = concat([
-      {
-        Effect = "Allow"
-        Action = [
-          "es:DescribeElasticsearchDomain",
-          "es:DescribeElasticsearchDomains",
-          "es:DescribeElasticsearchDomainConfig",
-          "es:ESHttpPost",
-          "es:ESHttpPut",
-          "opensearch:DescribeDomain",
-          "opensearch:DescribeDomains",
-          "opensearch:DescribeDomainConfig",
-          "opensearch:ESHttpPost",
-          "opensearch:ESHttpPut"
-        ]
-        Resource = [
-          var.opensearch_config.domain_arn,
-          "${var.opensearch_config.domain_arn}/*"
-        ]
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:AbortMultipartUpload",
-          "s3:GetBucketLocation",
-          "s3:GetObject",
-          "s3:ListBucket",
-          "s3:ListBucketMultipartUploads",
-          "s3:PutObject"
-        ]
-        Resource = [
-          aws_s3_bucket.backup.arn,
-          "${aws_s3_bucket.backup.arn}/*"
-        ]
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "logs:PutLogEvents"
-        ]
-        Resource = aws_cloudwatch_log_group.firehose_opensearch[0].arn
-      }
-    ], local.enable_transform ? [{
-      Effect = "Allow"
-      Action = [
-        "lambda:InvokeFunction"
-      ]
-      Resource = module.transform[0].function_arn
-    }] : [])
-  })
-}
-
-# CloudWatch log group for OpenSearch Firehose
-resource "aws_cloudwatch_log_group" "firehose_opensearch" {
-  count             = var.enable_opensearch ? 1 : 0
-  name              = "/aws/kinesisfirehose/${local.id}-opensearch"
-  retention_in_days = 7
-}
-
-resource "aws_cloudwatch_log_stream" "firehose_opensearch" {
-  count          = var.enable_opensearch ? 1 : 0
-  name           = "opensearch-delivery"
-  log_group_name = aws_cloudwatch_log_group.firehose_opensearch[0].name
-}
-
-resource "aws_cloudwatch_log_stream" "firehose_opensearch_s3" {
-  count          = var.enable_opensearch ? 1 : 0
-  name           = "s3-backup"
-  log_group_name = aws_cloudwatch_log_group.firehose_opensearch[0].name
 }
